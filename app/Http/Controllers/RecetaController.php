e97e9afa01ac375d7b59b8e049d593c339c11e5b
arcodev <arcodev07@gmail.com>
2021-08-10 22:06:38 -0500

    Agregando mas campos a la receta y creando la tabla de categorias

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\DB;
 
 class RecetaController extends Controller
 {
+    //Creacion de url's protegidas
+    public function __construct() {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
--- database/migrations/2021_08_06_082155_create_recetas_table.php
+++ database/migrations/2021_08_06_082155_create_recetas_table.php
@@ -13,9 +13,22 @@ class CreateRecetasTable extends Migration
      */
     public function up()
     {
+        //Creacion de la tabla receta dentro de la misma migracion
+        Schema::create('categoria_receta', function (Blueprint $table){
+            $table->id();
+            $table->string('nombre');
+            $table->timestamps();
+        });
+
         Schema::create('recetas', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
+            $table->text('ingredientes');
+            $table->text('preparacion');
+            $table->string('imagen');
+            //con foreignID apuntamos al ID de la tabla relacionada
+            $table->foreignID('user_id')->references('id')->on('users')->comment('El usuario que crea la receta');
+            $table->foreignID('categoria_id')->index('id')->on('categoria_receta')->comment('La categoria de la receta');
             $table->timestamps();
         });
     }
@@ -27,6 +40,7 @@ class CreateRecetasTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('categoria_receta');
         Schema::dropIfExists('recetas');
     }
 }
