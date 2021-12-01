333b8e6c76605fb7aac13533bc6bc4aa50534902
arcodev <arcodev07@gmail.com>
2021-12-01 13:04:01 -0600

    Creando relacion 1:1 entre categorias y recetas

--- app/Receta.php
+++ app/Receta.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Receta extends Model
 {
-    //
+    //Obtiene la categoria de la receta vía FK mediante una relacion 
+    public function categoria() 
+    {
+        return $this->belongsTo(CategoriaRecetas::class);
+    }
 }
